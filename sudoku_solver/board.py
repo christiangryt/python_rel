1640a06b9ddfbdf81aeb7010dc458ad17b98e228
christiangryt <dodif380@gmail.com>
2025-03-30 16:21:49 +0200

    viste sondre enkel introduksjon til pytohn

--- sudoku_solver/board.py
+++ sudoku_solver/board.py
@@ -21,7 +21,7 @@ class board(abstract_board):
 
     def detach(self, observer):
         return self.observers.remove(observer)
-    
+
     """
     Subscription management methods
     """
@@ -32,30 +32,28 @@ class board(abstract_board):
         """
 
         for observer in self.observers:
-            
+
             #Send board state to all observers
             observer.update(y, x, num)
-    
+
     """
     Solve Sudoku Methods
     """
 
     def generate_board(self, n):
         """
-        
         Set self._state to a n x n matrix with all 0s
-        
         """
 
         board = []
 
         for i in range(n):
-            
+
             #make nth column
             column = []
 
             for j in range(n):
-                
+
                 #add value k to nth colum
                 column.append(0)
 
@@ -69,9 +67,7 @@ class board(abstract_board):
 
     def check(self, board, row, col, num):
         """
-        
         Check if num at given location is valid. Does not check entire board, only relevant rows and columns.
-        
         """
 
         #check if row and col legal
@@ -80,7 +76,7 @@ class board(abstract_board):
             if board[row][i] == num or board[i][col] == num:
 
                 return False
-            
+
         #check if cell legal
         #TODO see how to make general form of sudoku other than 9x9
         #finner hvor man skal starte med indeks
@@ -95,14 +91,13 @@ class board(abstract_board):
 
 
         return True
-    
+
     def solve(self):
         """
-        
         Recusrive backtracking algorithm that solves the soduko. Updates Observers on board change Only looks at empty cells and does not overwrite existing data.
-        
         """
 
+        #TODO different iteration counts if board not normal size
         iter = len(self.state)
 
         for row in range(iter):
@@ -114,7 +109,7 @@ class board(abstract_board):
                     #test values 0-10
                     #TODO find way to have more than 10 numbers
                     for num in range(1,10):
-                        
+
                         #check if given num is legal
                         if self.check(self.state, row, col, num):
 
