bb75f654669379e5c46f3f8c35d61ddeecd3279f
christiangryt <dodif380@gmail.com>
2025-07-22 17:15:00 +0200

    began test work

--- flow/test.py
+++ flow/test.py
@@ -4,8 +4,16 @@ graph = [
     ["-","B","A"]
 ]
 
-
-
 for x in graph:
     for y in x:
         print (y)
+
+class flow():
+    """
+    Plan for self:
+    Represent a color (A->A). Calls path finding, this should be abstracted to more easily change the heuristics
+
+    (This pathfinding needs to be easily able to respect the restrictions given by the CBS. I do have to solve it several times, but alas)
+    """
+
+    None
